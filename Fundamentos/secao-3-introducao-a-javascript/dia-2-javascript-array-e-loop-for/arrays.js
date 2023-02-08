15193e2f2ff47d039c30ecb7c3e13cc881987a9d
lucaslangsch <lucas.langsch@gmail.com>
2023-02-08 15:11:54 -0300

    inicia frontend

---
+++ Fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/arrays.js
@@ -0,0 +1,16 @@
+// let pizzas = ['4 queijos', 'catupiry', 'calabresa']
+// console.log(pizzas);
+// pizzas[4] = 'frango caipira' // Adiciona o index 4 e ignora o 3
+// console.log(pizzas);
+// pizzas.push('da casa') // Adiciona após o último index, criando um novo último
+// console.log(pizzas);
+// console.log(pizzas.length); // Retorna o número de index do array
+// console.log(pizzas.sort()); // Retorna o array de forma ordenada. Modifica o array de forma permanente
+// console.log(pizzas[1]); // Retorna o index chamado
+// pizzas.unshift('marguerita') // Adiciona no index 0
+// console.log(pizzas);
+// pizzas.pop() // Exlcui o item do último index
+// console.log(pizzas);
+// pizzas.shift() // Exclui o item do primeiro index
+// console.log(pizzas);
+// console.log(pizzas.indexOf('calabresa')); // Retorna o index do elemento chamado
---
+++ Fundamentos/secao-3-introducao-a-javascript/dia-2-javascript-array-e-loop-for/for.js
@@ -0,0 +1,4 @@
+let pizzas = ['4 queijos', 'catupiry', 'calabresa']
+for (let i = 0; i < pizzas.length; i += 1) { // estrutura de repetição for, percorrendo do index 0 até o último
+  console.log(pizzas[i]);
+}
